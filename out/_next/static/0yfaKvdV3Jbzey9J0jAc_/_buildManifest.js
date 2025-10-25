7fb1f3c0f9d8e67a7336d1717bb7524f6e195cf2
sora03pt <sora03pt@yahoo.co.jp>
2025-10-25 22:32:18 +0900

    no message

---
+++ out/_next/static/0yfaKvdV3Jbzey9J0jAc_/_buildManifest.js
@@ -0,0 +1,23 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/a9b3f32022ca5071.js"
+  ],
+  "/_error": [
+    "static/chunks/43f80c95b94e757e.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio//_next/:path+",
+        "destination": "/portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
