1515857086dfc3457e70c0443404b5d7d63c4ddd
sora03pt <sora03pt@yahoo.co.jp>
2025-11-08 00:41:50 +0900

    no message

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,33 +2,35 @@ import Image from 'next/image';
 
 export default function About() {
 return (
-  <section className="relative md:flex items-center gap-10 bg-white rounded-3xl shadow-xl border border-gray-200 max-w-6xl mx-auto px-6 py-20">
-    <div className="flex-1 relative z-10">
-      <h2 className="text-3xl font-extrabold mb-6 text-gray-800 flex items-center gap-2">
-        <span className="inline-block w-2 h-6 bg-gray-400 rounded-full mr-2"></span>
-        About
-      </h2>
-      <p className="text-gray-700 leading-relaxed text-base md:text-lg mb-6">
-        <span className="text-gray-500">フロントエンド開発を中心に、見た目だけでなく使いやすさにもこだわった実装を行っています。<br />
-        現在はデザイン面からもアプローチできるよう、UI/UXデザインを学んでいます。</span>
-      </p>
-      <div className="flex flex-wrap gap-3 mt-4">
-        {["HTML", "CSS", "JS", "TS", "React", "Next.js", "Vue.js", "Photoshop"].map((skill) => (
-          <span key={skill} className="px-4 py-1 bg-gray-100 text-gray-700 border border-gray-300 rounded-full text-sm font-semibold shadow">
-            {skill}
-          </span>
-        ))}
+  <section className="max-w-6xl mx-auto px-6 py-20">
+    <div className="relative md:flex items-center gap-10 bg-gray-50 rounded-2xl shadow-sm border border-gray-600 max-w-6xl mx-auto px-6 py-20">
+      <div className="flex-1 relative z-10">
+        <h2 className="text-3xl font-extrabold mb-6 text-gray-800 flex items-center gap-2">
+          <span className="inline-block w-2 h-6 bg-gray-400 rounded-full mr-2"></span>
+          About
+        </h2>
+        <p className="text-gray-700 leading-relaxed text-base md:text-lg mb-6">
+          <span className="text-gray-500">フロントエンド開発を中心に、見た目だけでなく使いやすさにもこだわった実装を行っています。<br />
+          現在はデザイン面からもアプローチできるよう、UI/UXデザインを学んでいます。</span>
+        </p>
+        <div className="flex flex-wrap gap-3 mt-4">
+          {["HTML", "CSS", "JS", "TS", "React", "Next.js", "Vue.js", "Photoshop"].map((skill) => (
+            <span key={skill} className="px-4 py-1 bg-gray-100 text-gray-700 border border-gray-400 rounded-full text-sm font-semibold shadow">
+              {skill}
+            </span>
+          ))}
+        </div>
       </div>
-    </div>
-    <div className="mt-12 md:mt-0 flex justify-center items-center relative z-10">
-      <div className="w-50 h-50 rounded-full overflow-hidden shadow-lg border-4 border-white">
-        <Image
-          src="/portfolio/image/profile.jpg"
-          alt="Profile"
-          width={200}
-          height={200}
-          className="object-cover w-full h-full"
-        />
+      <div className="mt-12 md:mt-0 flex justify-center items-center relative z-10">
+        <div className="w-50 h-50 rounded-full overflow-hidden shadow-lg border-4 border-white">
+          <Image
+            src="/portfolio/image/profile.jpg"
+            alt="Profile"
+            width={200}
+            height={200}
+            className="object-cover w-full h-full"
+          />
+        </div>
       </div>
     </div>
   </section>
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -10,7 +10,7 @@ export default function Works() {
 const projects = [
 	{
 		title: "文章作成支援ツール(SP向け)",
-		desc: "v0を使って実装",
+		desc: "任意の値をセットして撮影会の募集文章を簡単に作成できるツール",
 		github: "https://github.com/sora03pt/cosplay-recruitment",
 		demo: "https://sora03pt.github.io/cosplay-recruitment/",
         image: '/portfolio/image/works/01.jpg',
@@ -31,22 +31,33 @@ const projects = [
 	},
 ];
 
+const designs = [
+	{
+		image: '/portfolio/image/works/design/01.jpg',
+		title: '名刺',
+	},
+	{
+		image: '/portfolio/image/works/design/02.jpg',
+		title: '英会話スクールバナー',
+	},
+	{
+		image: '/portfolio/image/works/design/03.jpg',
+		title: '夏期講習バナー',
+	},
+];
+
 const photos = [
 	{
 		image: '/portfolio/image/works/photo/01.jpg',
-		alt: '',
 	},
 	{
 		image: '/portfolio/image/works/photo/02.jpg',
-		alt: '',
 	},
 	{
 		image: '/portfolio/image/works/photo/03.jpg',
-		alt: '',
 	},
 	{
 		image: '/portfolio/image/works/photo/05.jpg',
-		alt: '',
 	},
 ];
 
@@ -58,9 +69,10 @@ return (
         {projects.map((p, i) => (
 		<div
 			key={i}
-			className={`bg-gray-50 rounded-2xl p-6 border border-gray-100 shadow-sm transition ${styles.card}`}
+			className={`bg-gray-50 rounded-2xl p-6 border border-gray-600 shadow-sm transition ${styles.card}`}
 		>
             <div className="h-32 rounded-xl mb-4">
+				<a href={p.demo}>
 				<Image
 					src={p.image}
 					alt="Project Image"
@@ -68,8 +80,9 @@ return (
 					height={128}
 					className="object-contain w-full h-full rounded-xl"
 				/>
+				</a>
             </div>
-            <h3 className="font-semibold">{p.title}</h3>
+            <h4 className="font-semibold">{p.title}</h4>
             <p className="text-sm text-gray-600 mt-1">{p.desc}</p>
             <div className="flex gap-2 mt-4">
                 <a
@@ -92,28 +105,51 @@ return (
         </div>
         ))}
     </div>
-		<h3 className="text-1xl font-semibold mb-4">Photo</h3>
-		<div className={`bg-gray-50 rounded-2xl p-6 border border-gray-100 shadow-sm ${styles.images}`}>
+	<h3 className="text-1xl font-semibold mb-4">design</h3>
+	<div className="grid md:grid-cols-3 gap-6 mb-16">
+		{designs.map((design, i) => (
+			<button
+				key={i}
+				type='button'
+				className={`bg-gray-50 rounded-2xl p-6 border border-gray-600 shadow-sm transition cursor-pointer ${styles.card}`}
+				onClick={() => {
+					setModalSrc(design.image);
+					setModalOpen(true);
+				}}
+			>
+				<img
+					src={design.image}
+					alt=""
+					height={360}
+					className={styles.item}
+				/>
+				<h4 className="font-semibold mt-3 text-left">{design.title}</h4>
+			</button>
+		))}
+		<ImageModal src={modalSrc} open={modalOpen} onClose={() => setModalOpen(false)} />
+	</div>
+	<h3 className="text-1xl font-semibold mb-4">Photo</h3>
+	<div className={`bg-gray-50 rounded-2xl p-6 border border-gray-600 shadow-sm ${styles.images}`}>
 		{photos.map((photo, i) => (
 			<button
 				key={i}
 				type='button'
 				className={styles.button}
 				onClick={() => {
-				  setModalSrc(photo.image);
-				  setModalOpen(true);
+					setModalSrc(photo.image);
+					setModalOpen(true);
 				}}
 			>
 				<img
 					src={photo.image}
-					alt={photo.alt}
+					alt=""
 					height={360}
 					className={styles.item}
 				/>
 			</button>
 		))}
 		<ImageModal src={modalSrc} open={modalOpen} onClose={() => setModalOpen(false)} />
-		</div>
+	</div>
 </section>
 );
 }
